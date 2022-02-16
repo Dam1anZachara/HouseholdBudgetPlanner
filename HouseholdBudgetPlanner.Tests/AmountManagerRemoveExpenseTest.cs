72a319f7a27839ebc26be184d41e6b8f49d4049e
Damian Z <damian9203@gmail.com>
2022-02-16 23:17:00 +0100

    Revision 4.0.1

--- HouseholdBudgetPlanner.Tests/AmountManagerRemoveExpenseTest.cs
+++ HouseholdBudgetPlanner.Tests/AmountManagerRemoveExpenseTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using HouseholdBudgetPlanner.App;
+using HouseholdBudgetPlanner.App.Concrete;
 using HouseholdBudgetPlanner.App.Managers;
 using HouseholdBudgetPlanner.Domain.Entity;
 using System;
@@ -18,25 +19,27 @@ namespace HouseholdBudgetPlanner.Tests
         public void ExpenseInAmountByDateListTest()
         {
             //Arrange
+            MenuActionService menuActionService = new MenuActionService();
             AmountService amountService = new AmountService();
-            AmountManager amountManager = new AmountManager();
+            AmountManager amountManager = new AmountManager(menuActionService, amountService);
+            AmountManagerRemoveExpense amountManagerRemoveExpense = new AmountManagerRemoveExpense(amountService, amountManager);
             Amount amountExpenseOne = new Amount() { Id = 1, Name = "General expenses", Date = new DateTime(2022, 02, 2), Value = 55.00m };
             Amount amountExpenseTwo = new Amount() { Id = 1, Name = "General expenses", Date = new DateTime(2022, 02, 3), Value = 45.00m };
             amountService.AddItem(amountExpenseOne);
             amountService.AddItem(amountExpenseTwo);
-            AmountManagerRemoveExpense amountManagerRemoveExpense = new AmountManagerRemoveExpense(amountService, amountManager);
-            
             var dateStart = new DateTime(2022, 02, 1);
             var dateEnd = new DateTime(2022, 02, 4);
-            var expectedOutputPattern = $"\r\nYour expenses since {dateStart} to {dateEnd}\r\n" + "2022:02:02 00:00:00; Name: General expenses; Value: 55.00PLN\r\n" + "2022:02:03 00:00:00; Name: General expenses; Value: 45.00PLN";
-            
+            var expectedOutputPattern = $"\r\nYour expenses since {dateStart} to {dateEnd}\r\n" + "2022-02-02 00:00:00; Name: General expenses; Value: 55.00PLN\r\n" + "2022-02-03 00:00:00; Name: General expenses; Value: 45.00PLN";
+            var testOutput = new StringWriter();
+            TextWriter textWriter;
+            textWriter = Console.Out;
+            Console.SetOut(testOutput);
+            var string1 = testOutput.ToString();
             //Act
             amountManagerRemoveExpense.ExpenseInAmountByDateList(dateStart, dateEnd);
-            var testOutput = new StringWriter();
-            Console.SetOut(testOutput);
-            var testOutputToString = testOutput.ToString();
+            //var testOutputToString = textWriter.ToString();
             //Assert
-            testOutputToString.Should().Contain(expectedOutputPattern);
+            string1.Should().Be(expectedOutputPattern);
 
         }
     }
