a6b80f6a7dcf9e8ea080110977384162216b59e4
Damian Z <damian9203@gmail.com>
2022-03-17 23:52:03 +0100

    Revision 5.3.3

---
+++ HouseholdBudgetPlanner.App/Concrete/AmountRaportListService.cs
@@ -0,0 +1,35 @@
+﻿using HouseholdBudgetPlanner.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseholdBudgetPlanner.App.Concrete
+{
+    public class AmountRaportListService
+    {
+        private List<Amount> _getListOfExpenses;
+        public AmountRaportListService()
+        {
+            _getListOfExpenses = new List<Amount>();
+        }
+        string pathFileRaport = $@"C:\Users\Damian\Desktop\{DateTime.Now.ToString("G")}BudgetBalanceRaport.csv";
+        public void Method1(List<Amount> getListOfExpenses, List<Amount> getListOfIncomes)
+        {
+            //using FileStream fs = File.Open(pathFileRaport, FileMode.Open, FileAccess.Read);
+            //byte[] buf = new byte[1024];
+            //int c;
+            //while ((c = fs.Read(buf, 0, buf.Length)) > 0)
+            //{
+            //    string text = Encoding.UTF8.GetString(buf, 0, c);
+            //}
+            //var lines = File.ReadLines(pathFileRaport);
+            using FileStream fs = File.Create(pathFileRaport);
+            using StreamWriter sw = new StreamWriter(fs);
+
+            //sw.Write
+        }
+    }
+}
--- HouseholdBudgetPlanner.App/Managers/BudgetStatusRaportManager.cs
+++ HouseholdBudgetPlanner.App/Managers/BudgetStatusRaportManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HouseholdBudgetPlanner.App.Abstract;
+using HouseholdBudgetPlanner.Domain.Entity;
+using HouseholdBudgetPlanner.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,47 @@ namespace HouseholdBudgetPlanner.App.Managers
 {
     public class BudgetStatusRaportManager : AmountManagerBudgetStatus
     {
+        private IService<Amount> _amountService;
+        private List<Amount> _amountsGetList;
+        private List<Amount> _raportExpenseList;
+        public BudgetStatusRaportManager(IService<Amount> amountService)
+        {
+            _amountService = amountService;
+            _amountsGetList = amountService.GetAllItems();
+            _raportExpenseList = new List<Amount>();
+        }
 
+        public void BudgetStatusRaportMethod()
+        {
+            var budgedStatusDateMenuKeyInfo = BudgetStatusDateMenu();
+            switch (budgedStatusDateMenuKeyInfo.KeyChar)
+            {
+                case '1':
+                    _raportExpenseList = RaportAllExpensesMonthList();
+
+                    break;
+                case '2':
+                    break;
+                default:
+                    Console.WriteLine("\r\nAction you entered does not exist\r\n");
+                    break;
+            }
+        }
+        public List<Amount> RaportAllExpensesMonthList() //private
+        {
+            Console.WriteLine($"\r\nYour expenses this month:\r\n");
+            var raportExpenseList = _amountsGetList.AsQueryable()
+                .Where(amount => DateTime.Now.Month == amount.Date.Month && DateTime.Now.Year == amount.Date.Year && amount.Id > 0)
+                .ToList();
+            return raportExpenseList;
+        }
+        public List<Amount> RaportAllIncomesMonthList() //private
+        {
+            Console.WriteLine($"\r\nYour expenses this month:\r\n");
+            var raportIncomeList = _amountsGetList.AsQueryable()
+                .Where(amount => DateTime.Now.Month == amount.Date.Month && DateTime.Now.Year == amount.Date.Year && amount.Id < 0)
+                .ToList();
+            return raportIncomeList;
+        }
     }
 }
